ccac44f8d70feae2707012ff93942ceb3e4534f9
Mohamed keita <keitamohamed12@gmail.com>
2022-09-05 02:05:49 -0400

    Init project

---
+++ src/test/java/com/keita/gleam/GleamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keita.gleam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GleamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
